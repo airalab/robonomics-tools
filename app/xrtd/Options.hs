273666a1e1c269c165831e0cd6d562e4bcac6a78
Alexander Krupenkin <mail@akru.me>
2018-12-14 12:44:13 +0300

    Switch to default gen4 lighthouse

--- app/xrtd/Options.hs
+++ app/xrtd/Options.hs
@@ -18,7 +18,7 @@ providerOptions = P.Config
     <$> option (HttpProvider <$> str) (long "web3" <> value infura <> metavar "URI" <> help "Ethereum node endpoint [DEFAULT: Infura mainnet]")
     <*> account
     <*> option str (long "ipfs" <> value "/ip4/127.0.0.1/tcp/5001" <> metavar "MULTIADDR" <> help "IPFS node endpoint [DEFAULT: localhost]")
-    <*> option str (long "lighthouse" <> value "airalab.lighthouse.3.robonomics.eth" <> metavar "ENS" <> help "Robonomics lighthouse name")
+    <*> option str (long "lighthouse" <> value "airalab.lighthouse.4.robonomics.eth" <> metavar "ENS" <> help "Robonomics lighthouse name")
     <*> option str (long "ens" <> value "0x314159265dD8dbb310642f98f50C066173C1259b" <> metavar "ADDRESS" <> help "ENS registry contract")
 
 account :: Parser PrivateKey
