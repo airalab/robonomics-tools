598827ed59b3cd88d8f99745d70d9bb523c98745
Alexander Krupenkin <mail@akru.me>
2019-02-19 11:46:30 +0300

    Switch to gen 5

--- app/xrtd/Options.hs
+++ app/xrtd/Options.hs
@@ -21,7 +21,7 @@ providerOptions = P.Config
     <$> option (HttpProvider <$> str) (long "web3" <> value infura <> metavar "URI" <> help "Ethereum node endpoint [DEFAULT: Infura mainnet]")
     <*> account
     <*> option str (long "ipfs" <> value "/ip4/127.0.0.1/tcp/5001" <> metavar "MULTIADDR" <> help "IPFS node endpoint [DEFAULT: localhost]")
-    <*> option str (long "lighthouse" <> value "airalab.lighthouse.4.robonomics.eth" <> metavar "ENS" <> help "Robonomics lighthouse name")
+    <*> option str (long "lighthouse" <> value "airalab.lighthouse.5.robonomics.eth" <> metavar "ENS" <> help "Robonomics lighthouse name")
     <*> option str (long "ens" <> value "0x314159265dD8dbb310642f98f50C066173C1259b" <> metavar "ADDRESS" <> help "ENS registry contract")
 
 account :: Parser LocalKey
@@ -36,6 +36,7 @@ chain = eitherReader go
     go "ropsten"    = Right ropsten
     go "kovan"      = Right kovan
     go "rikenby"    = Right rikenby
+    go "sidechain"  = Right 0x1163      -- Airalab sidechain
     go str | valid str && (read str :: Integer) > 0 = Right (read str)
            | otherwise = Left "CHAIN_ID shoud be [foundation, ropsten, kovan, rikenby] or positive integer"
     valid = all $ flip elem ("1234567890" :: String)
