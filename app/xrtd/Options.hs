ff2fd8dde7568495fd7c4a8d4a9be02955b319f9
Alexander Krupenkin <mail@akru.me>
2019-09-16 15:16:15 +0300

    xrtd: fix gasprice arg parsing (fix #7)

--- app/xrtd/Options.hs
+++ app/xrtd/Options.hs
@@ -33,12 +33,15 @@ providerOptions = P.Config
     <*> option str (long "lighthouse" <> value "airalab.lighthouse.5.robonomics.eth" <> metavar "ENS" <> help "Robonomics lighthouse name")
     <*> option str (long "factory" <> value "factory.5.robonomics.eth" <> metavar "ENS" <> help "Robonomics liability factory name")
     <*> option str (long "ens" <> value "0x314159265dD8dbb310642f98f50C066173C1259b" <> metavar "ADDRESS" <> help "ENS registry contract")
-    <*> option auto (long "gasprice" <> value (5 :: Szabo) <> metavar "GAS_PRICE" <> help "Transaction gas price, GWei [DEFAULT: 5]")
+    <*> option gasprice (long "gasprice" <> value (5 :: Szabo) <> metavar "GAS_PRICE" <> help "Transaction gas price, GWei [DEFAULT: 5]")
 
 account :: Parser LocalKey
 account = LocalKey . (importKey :: HexString -> PrivateKey)
     <$> option str (long "private" <> metavar "KEY" <> help "Hex encoded private key")
     <*> option chain (long "chain" <> value foundation <> metavar "CHAIN_ID" <> help "Ethereum chain [foundation, ropsten, kovan, rikenby]")
+
+gasprice :: ReadM Szabo
+gasprice = fromInteger <$> auto
 
 chain :: ReadM Integer
 chain = eitherReader go
